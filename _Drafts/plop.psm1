e2d8051452e4d807b71661de2e93f2a9bc98ac33
Stéphane van Gulick <svangulick@gmail.com>
2019-03-31 23:39:20 +0200

    final version before move to draft

--- _Drafts/plop.psm1
+++ _Drafts/plop.psm1
@@ -1,4 +1,3 @@
-
 Enum ComputerType {
     Server
     Client
@@ -7,14 +6,43 @@ Enum ComputerType {
 Class Computer {
     [String]$Name
     [ComputerType]$Type
+
+    Computer($type){
+        $this.Type = $type
+        $this.Name = $this.GetNewName()
+
+    }
+
+    [String]GetNewName(){
+        $Guid = [guid]::NewGuid()
+        $FullName = ''
+        switch ($this.type) {
+            'client' {
+                $FullName = 'CLT-' + $Guid 
+                break
+              }'Server' {
+                $FullName = 'SRV-' + $Guid 
+              }
+        }
+
+        return $FullName
+    }
 }
 
 Function Get-InternalStuff {
-    #Do internal stuff 
+    #Does internal stuff 
 }
 
 Function Get-ComputerData {
-    #Do stuff
+    #Does stuff<
 }
 
-Export-ModuleMember -Function Get-Data 
+Function New-Computer {
+    Param(
+        [ComputerType]$Type
+
+    )
+    [Computer]::New($Type)
+}
+
+Export-ModuleMember -Function Get-ComputerData,New-Computer
